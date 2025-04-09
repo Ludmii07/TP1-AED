c1bf97a35bd12d487bb44140487bd2fdea9dad4c
Ludmii07 <134977183+Ludmii07@users.noreply.github.com>
2025-04-09 20:47:59 -0300

    Add files via upload

---
+++ TP1.py
@@ -0,0 +1,30 @@
+destinatario = str(input("Ingrese el nombre y apellido del beneficiario: "))
+iso = str(input("Ingrese el código de identificación de la orden de pago, en formato 'AAA': "))
+montoNom = int(input("Ingrese el monto nominal: "))
+
+print("Beneficiario:", destinatario)
+moneda = ""
+montoBase = 0
+if "ARS" in iso:
+    moneda = "ARS"
+    montoBase = montoNom * 0.05
+
+elif "EUR" in iso:
+    moneda = "EUR"
+    montoBase = montoNom * 0.07
+elif "USD" in iso:
+    moneda = "USD"
+    montoBase = montoNom * 0.07
+elif "GBP" in iso:
+    moneda = "GBP"
+    montoBase = montoNom * 0.09
+elif "JPY" in iso:
+    moneda = "JPY"
+    montoBase = montoNom * 0.09
+else:
+    print("Moneda no autorizada")
+
+
+
+print("Moneda:", moneda)
+print("Monto base:", montoBase)
